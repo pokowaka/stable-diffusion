6cbcddf38a3113ea19c118c02046a2a1e025017a
James Reynolds <magnsuviri@me.com>
2022-08-26 13:46:36 -0600

    Changed default n_samples to 1 and n_iter to 1

--- scripts/txt2img.py
+++ scripts/txt2img.py
@@ -166,7 +166,7 @@ def main():
     parser.add_argument(
         "--n_iter",
         type=int,
-        default=2,
+        default=1,
         help="sample this often",
     )
     parser.add_argument(
@@ -196,7 +196,7 @@ def main():
     parser.add_argument(
         "--n_samples",
         type=int,
-        default=3,
+        default=1,
         help="how many samples to produce for each given prompt. A.k.a. batch size",
     )
     parser.add_argument(
